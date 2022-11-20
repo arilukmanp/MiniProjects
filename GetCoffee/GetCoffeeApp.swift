cf4bb4db4e713e319a214fcc7e35803cff4e7c58
Ari Lukman P <ari.lukman.p@gmail.com>
2022-11-20 12:13:12 +0700

    Initial Commit

---
+++ GetCoffee/GetCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetCoffeeApp.swift
+//  GetCoffee
+//
+//  Created by Ari on 20/11/22.
+//
+
+import SwiftUI
+
+@main
+struct GetCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
