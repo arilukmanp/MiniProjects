85e86f73ea9b48ffb3fe766e2de2c556a233d977
Ari Lukman P <ari.lukman.p@gmail.com>
2022-11-22 22:44:04 +0700

    Initial Commit

---
+++ Alohaa/AlohaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlohaaApp.swift
+//  Alohaa
+//
+//  Created by Ari on 22/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AlohaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ Alohaa/Views/HomeView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Alohaa
+//
+//  Created by Ari on 22/11/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
